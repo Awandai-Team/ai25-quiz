09776dfd4292bcfaaf44ffa8672b3284ab7c6f3f
tawanNophaket <tawaniphone2022@gmail.com>
2025-07-19 12:23:17 +0700

    logger api2

--- api2/main.py
+++ api2/main.py
@@ -1,22 +1,38 @@
 from fastapi import FastAPI
 import logging
+import os
+from logging.handlers import RotatingFileHandler
+
+""" **** ตั้งค่า Logger ****"""
+
+log_file_path = "../logs/api2.log"
+
+# Logger Instance
+logger = logging.getLogger("api2_logger")
+logger.setLevel(logging.INFO)
+
+# Handler เเบบหมุนเวียน (กันไฟล์เกิน)
+handler = RotatingFileHandler(log_file_path, maxBytes=2000, backupCount=5)
+
+# จัด Format
+formatter = logging.Formatter("%(asctime)s - API2 - %(levelname)s - %(message)s")
+
+# กำหนด Format ให้ Handler
+handler.setFormatter(formatter)
+
+# เพิ่ม Handler เข้า Logger
+logger.addHandler(handler)
+
+""" **** สร้าง FastAPI App ****"""
 
 app = FastAPI()  # สร้างเเอปฟลิเคชัน FastAPI
-
-# Config เเสดงผล Log เป็นระดับ INFO (ข้อมูลทั่วไป)
-# จัด format (ดึง เวลา, ชื่อระดับ, ข้อความหลัก)
-logging.basicConfig(
-    level=logging.INFO, format="%(asctime)s - API2 - %(levelname)s - %(message)s"
-)
 
 
 @app.get("/health")  # สร้าง endpoint ไปที่ path "/health"
 def read_root():
 
-    # เมื่อมี Request มาที่ endpoint นี้ จะทำงาน
-    logging.info(
-        "Request received. Sending response."
-    )  # Log เพื่อยืนยัน Status ว่า service นี้ถูกเรียกเเล้ว
+    # สั่งให้บันทึก Log
+    logger.info("Request received. Sending response.")
 
     # ส่ง JSON response
     return {"message": "Hello from API2", "status": "ok"}
